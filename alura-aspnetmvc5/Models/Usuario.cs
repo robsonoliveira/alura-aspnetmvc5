5c0285bcd248b0c496031b4778b891043c1ed573
Robson Oliveira <oliveira.robson.b@gmail.com>
2016-03-24 23:10:55 -0300

    Incluindo class Produto, Categoria e Usuario, com atributos do Produto

---
+++ alura-aspnetmvc5/Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace alura_aspnetmvc5.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public float Preco { get; set; }
+        public CategoriaDoProduto Categoria { get; set; }
+        public int? CategoriaId { get; set; }
+        public String Descricao { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
