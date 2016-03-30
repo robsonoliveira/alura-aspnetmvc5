df73adb1298e490b9baf1abdbbe7dc21b7007678
Robson B Oliveira <oliveira.robson.b@gmail.com>
2016-03-30 20:57:02 -0300

    Aula de autenticação de usuário - sesson

---
+++ alura-aspnetmvc5/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using alura_aspnetmvc5.DAO;
+using System.Web.Mvc;
+
+namespace alura_aspnetmvc5.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Autentica(string login, string senha) {
+            UsuariosDAO dao = new UsuariosDAO();
+            var usuario = dao.Busca(login, senha);
+            if(usuario != null)
+            {
+                Session["usuarioLogado"] = usuario;
+                return RedirectToAction("Index", "Produto");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+    }
+
+}
